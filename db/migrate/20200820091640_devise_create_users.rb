93032d9d5fd69a898cc929a9f8967a7cddc9a67d
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-20 12:07:31 +0000

    db migrate devise

--- db/migrate/20200820091640_devise_create_users.rb
+++ db/migrate/20200820091640_devise_create_users.rb
@@ -32,7 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :name
+      t.boolean :country
+      t.text :profile
+      t.string :image_id
       t.timestamps null: false
     end
 
