af02e1e82670fca1091b3951ad76edf2252c20b1
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-21 08:14:00 +0000

    Add post model

---
+++ db/migrate/20200821081059_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id
+      t.integer :like_id
+      t.integer :comment_id
+      t.text :text
+      t.string :image_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_091640) do
+ActiveRecord::Schema.define(version: 2020_08_21_081059) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "like_id"
+    t.integer "comment_id"
+    t.text "text"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
