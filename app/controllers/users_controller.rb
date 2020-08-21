cb9b280ff628099032582838e30fea0e5ba9e382
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-21 08:26:04 +0000

    Add users controller and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: "homes#top"
+  resources :posts
+  resources :users, only: [:show, :edit, :update]
 end
