17ae2c2386167868ca3c08800f3640f509e8825b
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-21 12:43:16 +0000

    Fix sign up form
    Add name and country radio button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,25 +4,36 @@ class PostsController < ApplicationController
   	@posts = Post.all
   end
 
+  def show
+  	@post = Post.find(params[:id])
+  	
+  end
+
   def new
   	@post = Post.new
   end
 
   def create
-  	@post = Post.new(post_params)
+  	post = Post.new(post_params)
   	# post.user_id = current_user.id
-  	@post.save
+  	post.save
   	redirect_to posts_path
   end
 
   def edit
+  	@post = Post.find(params[:id])
   end
 
   def update
-
+  	post = Post.find(params[:id])
+  	post.update(post_params)
+  	redirect_to post_path(post)
   end
 
   def destroy
+  	post = Post.find(params[:id])
+  	post.destroy
+  	redirect_to user_path(post)
 
   end
 
