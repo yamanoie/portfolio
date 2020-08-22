afa8519e479defd24687e631eda5332ea51fbdd1
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-22 07:51:10 +0000

    Add user function [show/edit/update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
 
 	def show
-		
+		@post = Post.find_by(id: params[:id])
+		@user = User.find_by(id: @post.user_id)
 	end
 
 	def edit
-		
+		@user = User.find(params[:id])
 	end
 
 	def update
-		
+		@user = User.find(params[:id])
+		@user.update(user_params)
+		redirect_to user_path(@user)
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:profile, :image, :name)
 	end
 end
