2363e8c06a466801b7665ad8d3838a1bde9fee87
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-22 04:23:33 +0000

    Change timezone UST into JST

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:country])
 	end
+
+	def after_sign_in_path_for(resource)
+    posts_path
+	end
+
+	def after_sign_up_path_for(resource)
+    posts_path
+	end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9eadcc4fcf5f90e7f60bb18209b5a4c7b94098315447063a3d510c7c3cdbd97bb268dd361558abde58948dc8eac4ed9c936080a0492c0b364346afc18f4aa30a'
