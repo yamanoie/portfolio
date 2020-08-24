43ea9a7f90fcc8a880a186e66c0612105fab2778
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-24 00:39:42 +0000

    Add Like funciton

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
-  resources :posts
+  resources :posts do
+  	resource :likes, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
