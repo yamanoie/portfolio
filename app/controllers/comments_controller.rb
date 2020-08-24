f1c06e053de28862354b25551fd6f22b7b86f691
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-24 05:02:54 +0000

    Fix Comment function
    change find_by to find method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,21 @@
 class CommentsController < ApplicationController
 
 	def create
+		post = Post.find(params[:post_id])
+		comment = current_user.comments.new(comment_params)
+		comment.post_id = post.id
+		comment.save
+		redirect_to post_path(post)
 	end
 
 	def destroy
+		comment = Comment.find(params[:id])
+		comment.destroy
+		redirect_to request.referer
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:comment)
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
 
 	def show
-		@post = Post.find_by(id: params[:id])
-		@user = User.find_by(id: @post.user_id)
+		@user = User.find(params[:id])
 		@posts = Post.where(user_id: @user.id)
 
 	end
