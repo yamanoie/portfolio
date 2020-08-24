f4ea6c3e686c2fe81798b9dd2c9d31b6d9347ac7
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-24 00:00:18 +0000

    Add Likes controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ApplicationRecord
 
 	belongs_to :user
+	has_many :likes, dependent: :destroy
 	attachment :image
 
+	def liked_by?(user)
+		likes.where(user_id: user.id).exists?
+	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :timeoutable
 
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
   attachment :image
 
 end
