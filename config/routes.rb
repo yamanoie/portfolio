813be0d8fd754a45d5e4a6266dad8c6887928639
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-24 00:52:16 +0000

    Add Comment controller and routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   resources :posts do
   	resource :likes, only: [:create, :destroy]
+  	resources :comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
 end
