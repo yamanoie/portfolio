b3c9ab9a3cd90c334a71313a74d3ec4246a1d1be
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-24 08:16:34 +0000

    Add runking

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,8 @@ class PostsController < ApplicationController
 
   def index
   	@posts = Post.all
+    @all_ranks = Post.find(Like.group(:post_id).order("count(post_id) desc").limit(3).pluck(:post_id))
+
   end
 
   def show
