c1c0740644128285c8b69c7680bc8c4c22573f67
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-26 01:44:30 +0000

    Add current(last)_sign_in_at column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_235934) do
+ActiveRecord::Schema.define(version: 2020_08_26_013931) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -92,6 +92,8 @@ ActiveRecord::Schema.define(version: 2020_08_25_235934) do
     t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
