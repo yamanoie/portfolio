6a17deda3de8bf64bf60ab8679fdb44bd729d195
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-28 05:02:26 +0000

    Add scroll fadein

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -57,3 +57,42 @@ $(function () {
         return false;
     });
 });
+
+
+
+// フェードイン
+
+$(window).scroll(function (){
+    $('.about-section').each(function(){
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+          if (scroll > elemPos - windowHeight + 100){
+              $(this).addClass('scrollin');
+            }
+        });
+    });
+
+
+$(window).scroll(function (){
+    $('.post-section').each(function(){
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+          if (scroll > elemPos - windowHeight + 100){
+              $(this).addClass('scrollin');
+            }
+        });
+    });
+
+
+$(window).scroll(function (){
+    $('.recruit-section').each(function(){
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+          if (scroll > elemPos - windowHeight + 100){
+              $(this).addClass('scrollin');
+            }
+        });
+    });
