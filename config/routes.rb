564d8f67cfc4d95cf3d6ba3460708ff4455063a8
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-28 07:43:20 +0000

    Create controller messages and rooms

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :matches, only: [:index, :update, :destroy, :create, :edit]
   resources :contacts, only: [:create]
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create, :show]
 end
