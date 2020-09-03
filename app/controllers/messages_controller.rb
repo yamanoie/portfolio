56b3294c0d2ed94544321dc4ef0f6cc3f91dc3fc
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-09-03 13:35:52 +0000

    Fix paginate at Messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,6 @@ class MessagesController < ApplicationController
 	def create
 		@message = Message.create(params.require(:message).permit(:message, :room_id).merge(user_id: current_user.id))
 		@message.create_notification_message!(current_user,@message.id,params[:message][:visited_id])#通知
-		# redirect_to "/rooms/#{@message.room_id}"
 		@room = Room.find(@message.room_id)
 		@messages = @room.messages.includes(:user).order(created_at: "DESC").page(params[:page]).per(8)
 		flash[:notice] = "Message has been sent"
@@ -12,7 +11,6 @@ class MessagesController < ApplicationController
 	def destroy
 		@message = Message.find(params[:id])
 		@message.destroy
-		# redirect_to request.referer
 		@room = Room.find(@message.room_id)
 		@messages = @room.messages.includes(:user).order(created_at: "DESC").page(params[:page]).per(8)
 		flash[:notice] = "Message has been deleted"
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,8 +9,8 @@ class RoomsController < ApplicationController
 
 	def show
 		@room = Room.find(params[:id])
-			@messages = @room.messages.includes(:user).order(created_at: "DESC").page(params[:page]).per(8)
-			@message = Message.new
-			# @entries = @room.entries.includes(:user)
+		@messages = @room.messages.includes(:user).order(created_at: "DESC").page(params[:page]).per(8)
+		@message = Message.new
+		# @entries = @room.entries.includes(:user)
 	end
 end
