6ccbf10e47d8fe92801dfe7d3162d37cec21c597
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-09-05 13:27:44 +0000

    Delete table entries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_110114) do
+ActiveRecord::Schema.define(version: 2020_09_05_132633) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -56,13 +56,6 @@ ActiveRecord::Schema.define(version: 2020_09_02_110114) do
     t.string "name"
     t.string "email"
     t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "entries", force: :cascade do |t|
-    t.integer "room_id"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
