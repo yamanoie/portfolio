01ca668fb24ee7e76dd5be3aaa06dfb5676ffdaa
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-09-10 09:15:04 +0000

    Tweal views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -123,6 +123,17 @@ $(function () {
 // フェードイン
 
 $(window).scroll(function (){
+    $('.mainvisual').each(function(){
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+          if (scroll > elemPos - windowHeight + 100){
+              $(this).addClass('scrollin');
+            }
+        });
+    });
+
+$(window).scroll(function (){
     $('.about-section').each(function(){
         var elemPos = $(this).offset().top,
             scroll = $(window).scrollTop(),
