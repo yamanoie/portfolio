ff5986ed094e06437a1faa0109e5bbc6a27e8176
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-09-10 12:54:43 +0000

    add Rails_ENV

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,9 +21,7 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/environment')
 set :output, 'log/crontab.log'
-# cronを実行する環境変数
 rails_env = ENV['RAILS_ENV'] || :development
-# cronを実行する環境変数をセット
 set :environment, rails_env
 
 every 1.day do
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 if Rails.env.development?
   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 end
+if Rails.env.production?
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+end
