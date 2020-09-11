50a1a72e927f1db94506a384af387897e625ce4a
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-09-11 12:24:34 +0000

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  get "home/welcome" => "homes#welcome"
   resources :posts do
     resource :likes, only: %i[create destroy]
     resources :comments, only: %i[create destroy]
