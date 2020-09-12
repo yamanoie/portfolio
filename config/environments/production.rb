4199a55341a723311b42073851c512b3299f8659
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-09-12 06:18:53 +0000

    chenge delivary error true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "segurado!_#{Rails.env}"
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
