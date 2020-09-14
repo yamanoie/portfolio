96cdb2923997d9af31b08ebdae94a741e120ea31
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-09-14 10:30:52 +0000

    Check comments delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-  # devise_for :users
   devise_for :users, :controllers => { :registrations => 'users/registrations', :sessions => 'users/sessions' }
   # devise_for :users, :controllers => {:registrations => 'users/registrations',:sessions => 'users/sessions'} 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
